fd157675c427d50c0091984f4a46b14d4692f98f
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-08-23 14:04:13 +0300

    Enable frontend

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+
+@app.get("/users/me")
+def get_user():
+    return {
+        "email": "example@example.com",
+        "isActive": True,
+        "profileId": "1",
+        "registeredAt": "2025-06-15T18:29:56+00:00",
+        "updatedAt": "2025-06-15T18:29:56+00:00",
+        "username": "user123",
+    }
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
